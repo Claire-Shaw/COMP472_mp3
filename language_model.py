b0bc9a9803cdd849a83b84bc191f73ab60555954
ClaireShaw <claireshaw17@gmail.com>
2018-11-25 16:50:58 -0500

    It works!!!

--- language_model.py
+++ language_model.py
@@ -22,7 +22,10 @@ class LanguageModel:
 
         ### Dictionaries for storing our unigram and bigram models
 		all_lowercase_letters = string.ascii_lowercase
-		two_letter_combos = list(itertools.product(all_lowercase_letters, all_lowercase_letters))
+		two_letter_combos = []
+		for combo in list(itertools.product(all_lowercase_letters, all_lowercase_letters)):
+			two_letter_combos.append(''.join(combo))
+
         # The unigram model will have 1 entry for each letter of the alphabet
 		self.unigram_model = dict.fromkeys(all_lowercase_letters, 0)
         # The bigram model will have 1 entry for each set of two letters in the alphabet
@@ -36,8 +39,8 @@ class LanguageModel:
 		self.generate_bigram_model(training_data)
 
 		### Dump trained models to output file
-		self.unigram_model_dump()
-		self.bigram_model_dump()
+		self.dump_unigram_model()
+		self.dump_bigram_model()
 
 
 	def generate_unigram_model(self, training_data):
@@ -45,7 +48,7 @@ class LanguageModel:
 
 		### Build our dict of letter counts
 		### At first, our model will contain the raw counts of letters seen in the training data
-		### This will be converted to log of probabilities later
+		### This will be converted to probabilities later
 		for token in list(training_data):
 			self.unigram_model[token] += 1
 
@@ -53,9 +56,9 @@ class LanguageModel:
 		total_letters = len(training_data)
 		total_bins = len(self.unigram_model)
 
-		### Convert our raw counts to log of probabilities (base 10) after performing add-delta smoothing
-		for key, value in self.unigram_model:
-			self.unigram_model[key] = math.log((value + self.delta) / (total_letters + self.delta * total_bins), 10)
+		### Convert our raw counts to probabilities after performing add-delta smoothing
+		for key, value in self.unigram_model.items():
+			self.unigram_model[key] = (value + self.delta) / (total_letters + self.delta * total_bins)
 		
 	
 	def generate_bigram_model(self, training_data):
@@ -66,29 +69,29 @@ class LanguageModel:
 
 		### Build our dict of bigram counts
 		### At first, our model will contain the raw counts of bigrams seen in the training data
-		### This will be converted to log of probabilities later
+		### This will be converted to probabilities later
 		for bigram in bigrams:
 			self.bigram_model[bigram] += 1
 			
 		total_bigrams = len(bigrams)
 		total_bins = len(self.bigram_model)
 
-		### Convert our raw counts to log of probabilities (base 10) after performing add-delta smoothing
-		for key, value in self.bigram_model:
-			self.bigram_model[key] = math.log((value + self.delta) / (total_bigrams + self.delta * total_bins), 10)
+		### Convert our raw counts to probabilities  after performing add-delta smoothing
+		for key, value in self.bigram_model.items():
+			self.bigram_model[key] = (value + self.delta) / (total_bigrams + self.delta * total_bins)
 	
 	
 	def dump_unigram_model(self):
 		with open(self.unigram_model_dump, 'w') as file:
-			for key, value in self.unigram_model:
-				file.write("P({}) = {}".format(key, value))
+			for key, value in self.unigram_model.items():
+				file.write("P({}) = {}\n".format(key, value))
 		print("{} unigram model has been dumped to {}".format(self.language_code, self.unigram_model_dump))
 
 
 	def dump_bigram_model(self):
 	    with open(self.bigram_model_dump, 'w') as file:
-		    for key, value in self.bigram_model:
-			    file.write("P({}|{}) = {}".format(key[1], key[0], value))
+		    for key, value in self.bigram_model.items():
+			    file.write("P({}|{}) = {}\n".format(key[1], key[0], value))
 	    print("{} bigram model has been dumped to {}".format(self.language_code, self.bigram_model_dump))
     
     
